73de9e47e60af662b57a4551e0f1ee2aca8b7dc8
CatPuncher90 <wittermarley@gmail.com>
2026-06-14 18:45:03 +0100

    network-first service worker for live updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fatscran-v1';
+const CACHE = 'fatscran-v2';
 const URLS = [
   '/fatscran/',
   '/fatscran/index.html',
@@ -22,5 +22,13 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/fatscran/index.html'))));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('/fatscran/index.html')))
+  );
 });
