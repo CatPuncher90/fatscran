518628a53a694b28ac7da2ad4e0c1b9f094c0be1
CatPuncher90 <wittermarley@gmail.com>
2026-06-20 19:55:33 +0100

    fix: nuke service worker cache, drop user_id not null

--- sw.js
+++ sw.js
@@ -1,34 +1,3 @@
-const CACHE = 'fatscran-v3';
-const URLS = [
-  '/fatscran/',
-  '/fatscran/index.html',
-  '/fatscran/recipe.html',
-  '/fatscran/shopping.html',
-  '/fatscran/planner.html',
-  '/fatscran/styles.css',
-  '/fatscran/recipes.js',
-  '/fatscran/fatscran-icon.png',
-  '/fatscran/fatscran-icon-small.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const resClone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, resClone));
-        return res;
-      })
-      .catch(() => caches.match(e.request).then(r => r || caches.match('/fatscran/index.html')))
-  );
-});
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
